141a4cc6d99b8b9986356968fad44147ac5af373
korolinakila <112556625+korolinakila@users.noreply.github.com>
2023-12-16 18:58:03 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -9,5 +9,11 @@ unsigned char itc_toUpper(unsigned char c);
 bool itc_compare(string s1, string s2);
 unsigned char itc_changeCase(unsigned char c);
 int itc_countWords(string str);
-
+string itc_maxCharWord(string str);
+bool slo (string s1);
+string pol(int pr1, int pr2, string S);
+char itc_sameChar(string str);
+bool itc_isFirstInSecond(string s1, string s2);
+string itc_Cezar(string str, int k);
+string itc_rmFreeSpace(string str);
 #endif // MIDDLE_STR_H_INCLUDED
